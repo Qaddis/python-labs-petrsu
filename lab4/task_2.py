b51e68cdf6b9e1979075e6c50220ff4ffa219fbe
Qaddis <svyatoslavb1107@gmail.com>
2025-11-14 15:51:42 +0300

    Finalize task 2 in lab4

--- lab4/task_2.py
+++ lab4/task_2.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
+x = np.linspace(-5, 5, 50)
+y = np.linspace(-5, 5, 50)
+
 X, Y = np.meshgrid(x, y)
 
 Z = np.sin(np.sqrt(X**2 + Y**2)) * np.exp(-(X**2 + Y**2))
 
-ax.plot_surface(X, Y, Z)
+ax.plot_surface(X, Y, Z, cmap="viridis")
 
 ax.set_xlabel("X")
 ax.set_ylabel("Y")

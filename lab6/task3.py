688a03af6227367a21db2053f6b1f8be54385070
Qaddis <svyatoslavb1107@gmail.com>
2025-11-15 16:54:39 +0300

    Add task 3 for lab6

---
+++ lab6/task3.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("test_img.png")
+
+avg_b, avg_g, avg_r, _ = cv2.mean(img)
+avg_color = (int(avg_b), int(avg_g), int(avg_r))
+
+square = np.full((100, 100, 3), avg_color, dtype=np.uint8)
+
+cv2.imshow("Average color square", square)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

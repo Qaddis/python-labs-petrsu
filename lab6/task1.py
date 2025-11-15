eb3db766cd4341a63557203edcb4386fc8ca5f21
Qaddis <svyatoslavb1107@gmail.com>
2025-11-15 16:13:35 +0300

    Add task 1 for lab6

---
+++ lab6/task1.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+size = 100
+
+img = np.zeros((size, size), dtype=np.uint8)
+
+for i in range(size):
+    for j in range(size):
+        if j >= i:
+            img[i, j] = int(255 * (1 - ((j / 2) / size)))
+        else:
+            img[i, j] = int((255 / 2) * (1 - (j / size)))
+
+cv2.imwrite("task1_img.png", img)
+
+cv2.imshow("Task 1 Lab6 Image:", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
